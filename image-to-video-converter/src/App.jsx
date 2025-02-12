22adf9006332141d75bcd9881e35d2fdb011302b
Arhaan Siddiquee <siddiqueearhaan@gmail.com>
2025-02-12 11:44:52 +0530

    commit

---
+++ image-to-video-converter/src/App.jsx
@@ -0,0 +1,10 @@
+// src/App.jsx
+import ImageToVideoConverter from "./components/ImageToVideo";
+
+export default function App() {
+  return (
+    <div className="flex justify-center items-center min-h-screen bg-gray-200">
+      <ImageToVideoConverter />
+    </div>
+  );
+}
